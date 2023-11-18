106e6264f6b9d84563288d6ec8314379a793b500
murillotlopes <murillo.tlopes@hotmail.com>
2023-11-18 13:02:24 -0300

    função util data-atual

---
+++ src/util/index.tsx
@@ -0,0 +1,8 @@
+
+export const dataAtual = () => {
+  const dataAtual = new Date();
+  const ano = dataAtual.getFullYear();
+  const mes = (dataAtual.getMonth() + 1).toString().padStart(2, '0');
+  const dia = dataAtual.getDate().toString().padStart(2, '0');
+  return `${ano}-${mes}-${dia}`;
+}
