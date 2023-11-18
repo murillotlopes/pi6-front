766361818e3a0efca649d71aecc345051228bfda
murillotlopes <murillo.tlopes@hotmail.com>
2023-11-18 13:14:20 -0300

    inclusão do PageBase

--- src/pages/Dashboard/dashboard.tsx
+++ src/pages/Dashboard/dashboard.tsx
@@ -1,23 +1,12 @@
-import { Link } from "react-router-dom"
+import { PageBase } from "../../components/pageBase/pageBase"
 
 export const Dashboard = () => {
   return (
     <>
-      <header>
-        <nav>
-          <Link to={'/'}>
-            Home
-          </Link>
-          <Link to={'/painel'}>
-            Painel
-          </Link>
-        </nav>
-      </header>
-      <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum quo consequatur odio enim perspiciatis voluptatibus ab laborum pariatur aliquid nulla. Odio exercitationem id dolores, aliquam sunt itaque quia quasi aperiam.</div>
-      <div>
-        SOU O PAINEL
-      </div>
-      <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum quo consequatur odio enim perspiciatis voluptatibus ab laborum pariatur aliquid nulla. Odio exercitationem id dolores, aliquam sunt itaque quia quasi aperiam.</div>
+      <PageBase>
+        <h2>DahsBoar</h2>
+        <p>Criar graficos aqui!</p>
+      </PageBase>
     </>
   )
 }
