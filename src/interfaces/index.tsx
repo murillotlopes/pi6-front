baa7809093aa1cc293a3398c9863e20b12b39516
murillotlopes <murillo.tlopes@hotmail.com>
2023-11-18 13:07:24 -0300

    adição de novos interfaces de tipagem do projeto

--- src/interfaces/index.tsx
+++ src/interfaces/index.tsx
@@ -5,6 +5,11 @@ export interface LoginType {
     senha: string
 }
 
+export interface UsuarioType {
+    nome: string,
+    email: string,
+}
+
 export interface CadastroLoginType {
     nome: string,
     email: string,
@@ -16,6 +21,54 @@ export interface childrenProps {
     children: ReactNode
 }
 
+export interface OperacaoType {
+    tipo_operacao: string,
+    data_operacao: string,
+    quantidade: number,
+    valor: number,
+
+    mercado?: string,
+    trade?: string,
+    corretagem?: string,
+    taxa_liquidacao?: number,
+    emolumentos?: number,
+    ir_retido?: number,
+    nota_corretagem?: number,
+    lucro_prejuizo?: number,
+
+    usuario?: UsuarioType,
+    investimento?: TituloType
+}
+
 export interface setEntrarOuCadastrarProps {
     setEntrarOuCadastrar: React.Dispatch<React.SetStateAction<boolean>>
 }
+
+export interface CardInvestimentoProps {
+    ativo: MeusInvestimentosType,
+    idKey: number
+}
+
+export interface TituloType {
+    id: number,
+    ticket: string,
+    nome_empresa: string,
+
+    cnpj?: string,
+    tipo?: string,
+    segmento?: string,
+    divident_yield?: string,
+    cota_rendimento?: number
+
+    operacoes?: Array<OperacaoType>
+}
+
+export interface MeusInvestimentosType {
+    ticket: string,
+    nome_empresa: string,
+    valor: number,
+    quantidade: number,
+    custo_medio: number,
+    recomendacao: string
+}
+
