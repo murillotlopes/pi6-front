bebcf54d11dfeea9d5d35c6b3097e7b6af342734
murillotlopes <murillo.tlopes@hotmail.com>
2023-11-18 13:08:15 -0300

    reorganização das rotas e criação da rota meus-investimentos

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,12 +1,16 @@
 import { Route, Routes } from "react-router-dom"
 import { Home } from "../pages/Home/home"
 import { Dashboard } from "../pages/Dashboard/dashboard"
+import { NovoInvestimento } from "../pages/NovoInvestimento/novoInvestimento"
+import { ListaInvestimento } from "../pages/ListaInvestimento/listaInvestimento"
 
 export const RoutesMain = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />}></Route>
-      <Route path="/painel" element={<Dashboard />}></Route>
+      <Route path="/" element={<Home />} />
+      <Route path="/painel" element={<Dashboard />} />
+      <Route path="/novo-investimento" element={<NovoInvestimento />} />
+      <Route path="/meus-investimentos" element={<ListaInvestimento />} />
     </Routes>
   )
 }
